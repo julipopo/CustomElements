15f33b240f1687db4eec35be3874052f5c6cdfeb
julien simmer <jsimmer@cardiweb.com>
2017-06-19 22:02:45 +0200

    add the first custom switch

---
+++ README.md
@@ -0,0 +1,13 @@
+# Custom Elements
+
+[![Platform](https://img.shields.io/cocoapods/p/LFAlertController.svg?style=flat)](http://cocoapods.org/pods/LFAlertController)
+[![Swift Version][swift-image]][swift-url]
+
+Native customized iOS elements
+
+#### Custom Switch
+<br>
+<img src="forGit/anim.gif" width="300px">  <img src="forGit/screenIB.png" width="300px">
+
+[swift-image]:https://img.shields.io/badge/swift-3.0-orange.svg
+[swift-url]: https://swift.org/
---
+++ Sources/CustomSwitch.swift
@@ -0,0 +1,96 @@
+//
+//  CustomSwitch.swift
+//  CustomSwitch
+//
+//  Created by julien simmer on 6/19/17.
+//  Copyright © 2017 julien simmer. All rights reserved.
+//
+
+import UIKit
+
+
+class CustomSwitch: UIView {
+
+    var imageStateOn : UIImage = UIImage()
+    var imageStateOff : UIImage = UIImage()
+    var backgroundImageView : UIImageView = UIImageView()
+    let borderBall : CGFloat = 4
+    var isOn : Bool = false
+    let ball = CALayer()
+    var h:CGFloat = 0
+    var w:CGFloat = 0
+    var ballSize : CGFloat = 0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    init(withImageOn : String, withImageOff : String, inRect: CGRect){
+        imageStateOn = UIImage.init(named: withImageOn)!
+        imageStateOff = UIImage.init(named: withImageOff)!
+        
+        super.init(frame: inRect)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        h = rect.height
+        w = rect.width
+        ballSize = h-2*borderBall
+        
+        backgroundImageView.image = imageStateOff
+        backgroundImageView.frame = CGRect(x: 0, y: 0, width: w, height: h)
+        self.addSubview(backgroundImageView)
+        
+        layer.cornerRadius = h/2
+        layer.masksToBounds = true
+        
+        ball.frame = CGRect(x: borderBall, y: borderBall, width: ballSize, height: ballSize)
+        ball.cornerRadius = ballSize/2
+        ball.backgroundColor = UIColor.groupTableViewBackground.cgColor
+        layer.addSublayer(ball)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(CustomSwitch.handleTap))
+        //tap.delegate = self as! UIGestureRecognizerDelegate
+        self.addGestureRecognizer(tap)
+    }
+    
+    func handleTap() {
+        isOn = !isOn
+        
+        UIView.animate(withDuration: 0.4, animations: {
+            self.ball.frame = self.isOn ? CGRect(x: self.w-self.ballSize-self.borderBall, y: self.borderBall, width: self.ballSize, height: self.ballSize) : CGRect(x: self.borderBall, y: self.borderBall, width: self.ballSize, height: self.ballSize)
+            self.backgroundImageView.image = self.isOn ? self.imageStateOn : self.imageStateOff
+        })
+    }
+}
+
+extension CustomSwitch {
+    @IBInspectable var onImage: UIImage? {
+        get {
+            return imageStateOn
+        }
+        set {
+            imageStateOn = newValue!
+        }
+    }
+    @IBInspectable var offImage: UIImage? {
+        get {
+            return imageStateOff
+        }
+        set {
+            imageStateOff = newValue!
+        }
+    }
+    @IBInspectable var value: Bool? {
+        get {
+            return isOn
+        }
+        set {
+            isOn = newValue!
+        }
+    }
+}
